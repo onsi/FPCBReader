55a043f49124717b6fac0c8959e102dafd845e7b
Onsi Fakhouri <onsijoe@gmail.com>
2011-11-24 09:36:28 -0800

    Initial Commit

---
+++ FPCBReader/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FPCBReader
+//
+//  Created by Onsi Fakhouri on 11/24/11.
+//  Copyright (c) 2011 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
