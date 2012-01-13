c414c20985174049c9a0709fb3162d8dca2c30a3
Onsi Fakhouri <onsijoe@gmail.com>
2012-01-13 07:52:56 -0800

    Latest readings

--- App/Domain/CachedPassage.m
+++ App/Domain/CachedPassage.m
@@ -57,7 +57,7 @@
 + (void)ensureCacheLimit {
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"CachedPassage"];
     NSUInteger count = [reader.dataManager.managedObjectContext countForFetchRequest:request error:NULL];
-    if (count < 30) {
+    if (count < 10) {
         return;
     }
     
